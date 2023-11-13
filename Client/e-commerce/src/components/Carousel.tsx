e5b316b385ca38792b7180a6311b3342f3c17bb8
Alejandro Medina <ale_m@outlook.com>
2023-11-13 11:28:35 -0300

    url back

--- Client/e-commerce/src/components/Carousel.tsx
+++ Client/e-commerce/src/components/Carousel.tsx
@@ -5,7 +5,7 @@ interface props {
 
 function Carousel({ arrayImages, autoPlay }: props) {
     return (
-        <article className="d-flex justify-content-center align-items-center d-lg-none">
+        <article className="d-flex justify-content-center align-items-center d-lg-none" >
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-theme="light" data-bs-ride={autoPlay ? "carousel" : "false"} >
                 <div className="carousel-inner" >
                     <div className="carousel-indicators">
@@ -20,16 +20,16 @@ function Carousel({ arrayImages, autoPlay }: props) {
                             ></button>
                         ))}
                     </div>
-                    <div className="carousel-item active" data-bs-interval="5500">
-                        <img src={arrayImages[0]} className="d-block w-100" alt="Imagen del producto 1" />
+                    <div className="carousel-item active " style={{maxHeight:"60vh", minHeight:"60vh"}} data-bs-interval="5500">
+                        <img src={arrayImages[0]}  className="d-block img-fluid" alt="Imagen del producto 1" />
                     </div>
                     {
                         arrayImages.slice(1).map((image: string, index: number) => {
                             return (
                                 <div
                                     key={image}
-                                    className="carousel-item" data-bs-interval="5500">
-                                    <img src={image} className="d-block w-100 " alt={`Imagen del producto ${index + 2}`} />
+                                    className="carousel-item " style={{maxHeight:"60vh", minHeight:"60vh"}} data-bs-interval="5500">
+                                    <img src={image}  className="d-block img-fluid" alt={`Imagen del producto ${index + 2}`} />
                                 </div>
                             )
                         })
