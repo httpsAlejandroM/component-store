7467f44328b43c7163b5538aa93cd25630b6dcca
Alejandro Medina <ale_m@outlook.com>
2023-12-25 20:24:35 -0300

    add outlet dashboard layout

--- Client/e-commerce/src/App.tsx
+++ Client/e-commerce/src/App.tsx
@@ -5,8 +5,8 @@ import RouteNotFound from "./components/RouteNotFound";
 import { PrivateRoutes, PublicRoutes } from "./utilities/routes";
 import { checkAuth } from "./redux/slices/user.slice";
 import { useAppDispatch } from "./redux/hooks";
-const Footer = lazy(() => import('./components/Footer/Footer'));
-const Navbar = lazy(() => import('./components/Navbar/Navbar'));
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 const Home = lazy(() => import('./views/Home/Home'));
 const Shop = lazy(() => import('./views/Shop/Shop'));
 const Detail = lazy(() => import('./views/Detail/Detail'));
@@ -15,6 +15,12 @@ const SignUp = lazy(() => import("./components/Auth/SignUp"))
 const Login = lazy(() => import("./components/Auth/Login"))
 const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"))
 const AuthGuard = lazy(() => import("./components/AuthGuard"))
+const Compras = lazy(() => import("./components/Dashboard/UserSections/Compras"))
+const Opiniones = lazy(() => import("./components/Dashboard/UserSections/Opiniones"))
+const Favoritos = lazy(() => import("./components/Dashboard/UserSections/Favoritos"))
+const Carrito = lazy(() => import("./components/Dashboard/UserSections/Carrito"))
+const Soporte = lazy(() => import("./components/Dashboard/UserSections/Soporte"))
+const MiPerfil = lazy(() => import("./components/Dashboard/UserSections/MiPerfil"))
 
 function App() {
   const dispatch = useAppDispatch()
@@ -22,7 +28,7 @@ function App() {
   useEffect(() => {
     dispatch(checkAuth())
   }, [])
-  
+
   return (
     <>
       <Suspense fallback={<Loader />}>
@@ -35,8 +41,15 @@ function App() {
           <Route path={PublicRoutes.SUPPORT} element={<Support />} />
           <Route path={PublicRoutes.SIGNUP} element={<SignUp />} />
           <Route path={PublicRoutes.LOGIN} element={<Login />} />
-          <Route element={<AuthGuard />}>
-            <Route path={PrivateRoutes.DASHBOARD+"/*"} element={<Dashboard />}/>
+          <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />}>
+            <Route element={<AuthGuard />}>
+              <Route index path={PrivateRoutes.DASHBOARD_SHOPPING} element={<Compras />} />
+              <Route path={PrivateRoutes.DASHBOARD_REVIEWS} element={<Opiniones />} />
+              <Route path={PrivateRoutes.DASHBOARD_FAVORITES} element={<Favoritos />} />
+              <Route path={PrivateRoutes.DASHBOARD_CART} element={<Carrito />} />
+              <Route path={PrivateRoutes.DASHBOARD_SUPPORT} element={<Soporte />} />
+              <Route path={PrivateRoutes.DASHBOARD_MY_PROFILE} element={<MiPerfil />} />
+            </Route>
           </Route>
         </Routes>
         <Footer />
--- Client/e-commerce/src/components/Dashboard/Dashboard.tsx
+++ Client/e-commerce/src/components/Dashboard/Dashboard.tsx
@@ -1,34 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import { PrivateRoutes } from "../../utilities/routes";
-import { lazy, Suspense } from "react";
-import Loader from "../Loader";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-const Compras = lazy(()=> import("./UserSections/Compras"))
-const Opiniones = lazy(()=> import("./UserSections/Opiniones"))
-const Favoritos = lazy(()=>import("./UserSections/Favoritos"))
-const Carrito = lazy(()=>import("./UserSections/Carrito"))
-const Soporte = lazy(()=>import("./UserSections/Soporte"))
-const MiPerfil = lazy(()=>import("./UserSections/MiPerfil"))
 
 function Dashboard() {
-//AGREGAR LAZY LOADING
 //AGREGAR/SACAR FAVORITOS DE BACKEND
 //RESPONSE FAVORITE COMPONENT
     
     return (
-      <Suspense fallback={<Loader/>}>
         <main className="min-vh-100 d-flex flex-row content">
            <Sidebar />
-          <Routes>
-            <Route path={PrivateRoutes.DASHBOARD_SHOPPING} element={<Compras/>}/>
-              <Route path={PrivateRoutes.DASHBOARD_REVIEWS} element={<Opiniones />} />
-              <Route path={PrivateRoutes.DASHBOARD_FAVORITES}  element={<Favoritos />}/>
-              <Route path={PrivateRoutes.DASHBOARD_CART} element={<Carrito />} />
-              <Route path={PrivateRoutes.DASHBOARD_SUPPORT}  element={<Soporte />}/>
-              <Route path={PrivateRoutes.DASHBOARD_MY_PROFILE} element={<MiPerfil />} />
-          </Routes>
+           <Outlet/>
         </main>
-        </Suspense>
     )
 }
 export default Dashboard
