068e86a96b3a5b5a96da9e063b4e8cb3bbd0960a
Alejandro Medina <ale_m@outlook.com>
2023-12-25 22:07:42 -0300

    roll back authguard

--- Client/e-commerce/src/components/AuthGuard.tsx
+++ Client/e-commerce/src/components/AuthGuard.tsx
@@ -1,17 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom"
 import { useAppSelector } from "../redux/hooks"
 import { PublicRoutes } from "../utilities/routes"
-import Loader from "./Loader"
 
 function AuthGuard() {
   const userInfo = useAppSelector((state) => state.userReducer)
-
-  if (userInfo.loading) {
-    return <Loader />
-  }
-  else {
-    return userInfo.isAuthenticated ? <Outlet /> : <Navigate to={PublicRoutes.LOGIN} />
-  }
+  
+  return  userInfo.isAuthenticated ? <Outlet /> : <Navigate to={PublicRoutes.LOGIN} />
   
 }
 export default AuthGuard
--- Client/e-commerce/src/components/Dashboard/Dashboard.tsx
+++ Client/e-commerce/src/components/Dashboard/Dashboard.tsx
@@ -4,10 +4,10 @@ import Sidebar from "./Sidebar";
 function Dashboard() {
 //AGREGAR/SACAR FAVORITOS DE BACKEND
 //RESPONSE FAVORITE COMPONENT
-    
+
     return (
         <main className="min-vh-100 d-flex flex-row content">
-           <Sidebar />
+          <Sidebar /> 
            <Outlet/>
         </main>
     )
