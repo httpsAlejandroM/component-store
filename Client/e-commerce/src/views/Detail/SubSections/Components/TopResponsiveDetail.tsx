40f06798d1913e6efe902d15812f044a50d70ff9
Alejandro Medina <ale_m@outlook.com>
2023-12-30 22:42:17 -0300

    fix top responsive detail component

--- Client/e-commerce/src/views/Detail/SubSections/Components/TopResponsiveDetail.tsx
+++ Client/e-commerce/src/views/Detail/SubSections/Components/TopResponsiveDetail.tsx
@@ -1,4 +1,5 @@
 import { ComponentInterface } from "../../../../interfaces"
+import { useAppSelector } from "../../../../redux/hooks"
 import FavButton from "./FavButton"
 import StarsRating from "./StarsRating"
 
@@ -6,20 +7,24 @@ interface props {
     data: ComponentInterface
 }
 
-function TopResponsiveDetail({data}:props) {
-  return (
-    <div className="d-flex flex-column d-lg-none">
-                <div className="d-flex align-items-center justify-content-between">
+function TopResponsiveDetail({ data }: props) {
+
+    const userInfo = useAppSelector((state) => state.userReducer)
+    const isFav = userInfo.userInfo?.favorites?.some((product) => product._id == data._id)
+
+    return (
+        <div className="d-flex flex-column d-lg-none">
+            <div className="d-flex align-items-center justify-content-between">
                 <div className="d-flex flex-row fs-5 text-warning align-items-center mt-2">
                     <span className="me-3 mb-0 text-dark p-0">4.5</span>
                     <StarsRating review={4}></StarsRating>
                     <span className="text-dark fs-6">(1)</span>
                 </div>
-                    <FavButton styles="d-flex justify-content-end"></FavButton>
-                </div>
-                <h2 className="d-flex text-dark display-5 text-wrap text-truncate mb-0">{data.title}</h2>
+                <FavButton componentFav={data} isFav={isFav} styles="d-flex justify-content-end"></FavButton>
             </div>
-  )
+            <h2 className="d-flex text-dark display-5 text-wrap text-truncate mb-0">{data.title}</h2>
+        </div>
+    )
 }
 
 export default TopResponsiveDetail
