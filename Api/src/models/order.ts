f61a022be69b74224b220839f394555297787968
Alejandro Medina <ale_m@outlook.com>
2024-03-15 23:25:44 -0300

    fix order model

--- Api/src/models/order.ts
+++ Api/src/models/order.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 import OrderInterface, { StatusDetail } from "../interfaces/order.interface";
 
 const orderSchema = new Schema<OrderInterface>({
-    _id:{
+    id:{
         type: Number,
         required: true,
         unique: true
