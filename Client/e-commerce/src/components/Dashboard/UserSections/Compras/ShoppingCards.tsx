2870139fdc66eba15d70767fc69855d7c24a636c
Alejandro Medina <ale_m@outlook.com>
2024-05-17 20:50:21 -0300

    fix responsive shoppin card

--- Client/e-commerce/src/components/Dashboard/UserSections/Compras/ShoppingCards.tsx
+++ Client/e-commerce/src/components/Dashboard/UserSections/Compras/ShoppingCards.tsx
@@ -28,26 +28,26 @@ function ShoppingCard({ items, statusDetail, datePayment, total }: orderInterfac
                         <div className="" key={component.id}>
                             <div className="row d-flex flex-row p-3 p-md-5 align-items-center justify-content-center justify-content-lg-evenly">
                                 
-                                <div className="row col-4 offset-1 offset-md-0 col-lg-2 p-0">
+                                <div className="row col-4 col-md-3 offset-md-1 offset-lg-0 col-lg-2 p-0">
                                     <Link to={`${PublicRoutes.DETAIL}/${component.id}`}>
                                         <img className="img-fluid" src={component.picture_url} alt="" />
                                     </Link>
 
                                 </div>
 
-                                <div className="row col-8 col-lg-5 ps-4 d-flex  flex-column justify-content-evenly align-items-center">
+                                <div className="row col-8  col-lg-5 ps-4 d-flex  flex-column justify-content-evenly align-items-center">
                                     <p className={`row ${textColor} fs-6`}>{statusDetail}</p>
                                     <p className="row fs-6">{component.title}</p>
                                     <small className="row">{`${component.quantity} ${component.quantity > 1? "Unidades" : "Unidad"}`}</small>
                                 </div>
 
-                                <div className="row col-2 col-lg-2 fs-4 d-none d-md-flex flex-row justify-content-center align-items-center">
+                                <div className="row col-2 col-lg-2 fs-4 d-none d-lg-flex flex-row justify-content-center align-items-center">
                                     {`$${component.unit_price}`}
                                 </div>
 
                                { 
                                index === items.length -1
-                               ?<div className="row col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center gap-2 mt-4 mt-md-0">
+                               ?<div className="row col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center gap-2 mt-4 mt-lg-0">
                                     <Link to={`${PublicRoutes.DETAIL}/${component.id}`} className="btn btn-buy shadow-sm col-10">
                                         Ver compra
                                     </Link>
